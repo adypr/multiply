2da281fe3ffa29eb7893f57ea251af04f0359f2d
Adypr <workady2015@gmail.com>
2019-10-12 00:20:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let num1 = BigInt(first);
+  let num2 = BigInt(second);
+  let res = String(num1 * num2);
+  return res;
 }
+
+
+//console.log(multiply('76095476904769456798433465783', '32986584375683268326587346582436583456'));
